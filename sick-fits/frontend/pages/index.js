2c30da1760ae741cf53866755c1463fbed79e77e
koy <kosatun@aol.com>
2019-11-29 18:02:02 -0800

    created Home, Header, Page components and styled with styled-components

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+const Home = props => {
+  return (
+    <div>
+      <p>Hello Home page </p> 
+    </div>
+  )
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Sell = props => {
+  return (
+    <div>
+      <p>Hello Sell page </p> 
+    </div>
+  )
+}
+
+export default Sell;
